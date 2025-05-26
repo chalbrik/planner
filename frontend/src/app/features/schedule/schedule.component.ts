b51190bcab18d8890b404dc465011b9c5309e4bf
Paweł Swacha <pawelswacha@mac-mini-pawe.home>
2025-05-26 21:22:38 +0200

    Update changes

--- frontend/src/app/features/schedule/schedule.component.ts
+++ frontend/src/app/features/schedule/schedule.component.ts
@@ -2,6 +2,15 @@ import {Component, OnInit, signal} from '@angular/core';
 import {ScheduleService} from '../../core/services/schedule.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import {
+  MatCell,
+  MatFooterCell,
+  MatFooterRow,
+  MatHeaderCell,
+  MatHeaderRow,
+  MatRow,
+  MatTable
+} from '@angular/material/table';
 
 interface Day {
   date: Date;
@@ -12,7 +21,7 @@ interface Day {
 
 @Component({
   selector: 'app-schedule',
-  imports: [FormsModule, ReactiveFormsModule, CommonModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule, MatTable, MatHeaderCell, MatCell, MatFooterCell, MatHeaderRow, MatRow, MatFooterRow, MatButtonToggleGroup, MatButtonToggle],
   templateUrl: './schedule.component.html',
   styleUrl: './schedule.component.scss',
   standalone: true,
@@ -26,6 +35,12 @@ export class ScheduleComponent implements OnInit {
   currentMonth = signal<Date>(new Date());
   days = signal<Day[]>([]);
 
+  // Zmienne tabeli
+  displayedColumns: string[] = [];
+  dataSource = ELEMENT_DATA;
+
+  tables = [0];
+
   constructor(private scheduleService: ScheduleService) {}
 
   ngOnInit() {
@@ -34,6 +49,22 @@ export class ScheduleComponent implements OnInit {
 
     console.log("Pracownicy: ", this.employees);
     console.log("Godziny pracy: ", this.workHours);
+
+
+    // Zmienne do tabeli
+    this.displayedColumns.length = 24;
+    this.displayedColumns.fill('filler');
+
+    // The first two columns should be position and name; the last two columns: weight, symbol
+    this.displayedColumns[0] = 'position';
+    this.displayedColumns[1] = 'name';
+    this.displayedColumns[22] = 'weight';
+    this.displayedColumns[23] = 'symbol';
+
+    /** Whether the button toggle group contains the id as an active value. */
+    isSticky(buttonToggleGroup: MatButtonToggleGroup, id: string) {
+      return (buttonToggleGroup.value || []).indexOf(id) !== -1;
+    }
   }
 
   loadEmployees(){
@@ -66,3 +97,23 @@ export class ScheduleComponent implements OnInit {
     });
   }
 }
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
