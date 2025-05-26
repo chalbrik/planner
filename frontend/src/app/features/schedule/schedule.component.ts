378618f57a57a6f1c0f9535ef43f89da9d17ef4f
Paweł Swacha <pawelswacha@mac-mini-pawe.home>
2025-05-26 22:18:16 +0200

    Update changes

--- frontend/src/app/features/schedule/schedule.component.ts
+++ frontend/src/app/features/schedule/schedule.component.ts
@@ -1,17 +1,18 @@
-import {Component, OnInit, signal} from '@angular/core';
+import {Component, OnInit, signal, ViewEncapsulation} from '@angular/core';
 import {ScheduleService} from '../../core/services/schedule.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import {
-  MatCell, MatColumnDef,
-  MatFooterCell,
-  MatFooterRow,
-  MatHeaderCell,
-  MatHeaderRow,
-  MatRow,
+  MatCell, MatCellDef, MatColumnDef,
+  MatFooterCell, MatFooterCellDef,
+  MatFooterRow, MatFooterRowDef,
+  MatHeaderCell, MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef,
+  MatRow, MatRowDef,
   MatTable
 } from '@angular/material/table';
 import {MatButtonToggle, MatButtonToggleGroup} from '@angular/material/button-toggle';
+import {MatButton} from '@angular/material/button';
 
 interface Day {
   date: Date;
@@ -35,11 +36,19 @@ interface Day {
     MatFooterRow,
     MatButtonToggleGroup,
     MatButtonToggle,
-    MatColumnDef
+    MatColumnDef,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatFooterCellDef,
+    MatFooterRowDef,
+    MatRowDef,
+    MatHeaderRowDef,
+    MatButton
   ],
   templateUrl: './schedule.component.html',
   styleUrl: './schedule.component.scss',
   standalone: true,
+  encapsulation: ViewEncapsulation.None,
 })
 export class ScheduleComponent implements OnInit {
   employees: any[] = [];
@@ -67,14 +76,14 @@ export class ScheduleComponent implements OnInit {
 
 
     // Zmienne do tabeli
-    this.displayedColumns.length = 24;
+    this.displayedColumns.length = 10;
     this.displayedColumns.fill('filler');
 
-    // The first two columns should be position and name; the last two columns: weight, symbol
-    this.displayedColumns[0] = 'position';
+    // The first two columns should be employees and name; the last two columns: weight, symbol
+    this.displayedColumns[0] = 'employees';
     this.displayedColumns[1] = 'name';
-    this.displayedColumns[22] = 'weight';
-    this.displayedColumns[23] = 'symbol';
+    this.displayedColumns[6] = 'weight';
+    this.displayedColumns[9] = 'symbol';
 
   }
 
@@ -117,20 +126,20 @@ export class ScheduleComponent implements OnInit {
 
 export interface PeriodicElement {
   name: string;
-  position: number;
+  employees: string;
   weight: number;
   symbol: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {employees: "Jan Kowalski", name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {employees: "Jan Kowalski", name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {employees: "Jan Kowalski", name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {employees: "Jan Kowalski", name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {employees: "Jan Kowalski", name: 'Boron', weight: 10.811, symbol: 'B'},
+  {employees: "Jan Kowalski", name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {employees: "Jan Kowalski", name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {employees: "Jan Kowalski", name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {employees: "Jan Kowalski", name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {employees: "Jan Kowalski", name: 'Neon', weight: 20.1797, symbol: 'Ne'},
 ];
